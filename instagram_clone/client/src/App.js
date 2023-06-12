007648a3883e0f815f9dd6da41768c3bf63b603c
kimeumpyo <eumpyo0409@gmail.com>
2023-06-12 21:38:12 +0900

    Front

--- instagram_clone/client/src/App.js
+++ instagram_clone/client/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthProvider from './components/AuthProvider';
+import AuthRequired from "./components/AuthRequired";
+import Layout from "./components/Layout";
+import Feed from "./components/Feed";
+import ArticleView from "./components/ArticleView";
+import Comments from "./components/Comments";
+import Search from "./components/Search";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Profile from "./components/Profile";
+import FollowerList from "./components/FollowerList";
+import FollowingList from "./components/FollowingList";
+import Accounts from "./components/Accounts";
+import NotFound from "./components/NotFound";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={
+            <AuthRequired>
+              <Layout />
+            </AuthRequired>
+          }>
+            <Route index element={<Feed />} />
+            <Route path="search" element={<Search />} />
+            <Route path="p/:id">
+              <Route index element={<ArticleView />} />
+              <Route path="comments" element={<Comments />} />
+            </Route>
+            <Route path="profiles/:username">
+              <Route index element={<Profile />} />
+              <Route path="followers" element={<FollowerList />} />
+              <Route path="following" element={<FollowingList />} />
+            </Route>
+            <Route path="accounts/edit" element={<Accounts />} />
+          </Route>
+          <Route path="accounts/login" element={<Login />} />
+          <Route path="accounts/signup" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  )
 }
 
-export default App;
+
--- instagram_clone/client/src/components/NotFound.js
+++ instagram_clone/client/src/components/NotFound.js
@@ -1,1 +1,19 @@
-export default function NotFound(){}
+import { useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+
+  useEffect(() => {
+    document.title = `Page not found - Instagram`;
+  }, [])
+
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-semibold my-4 text-center">페이지를 찾을 수 없습니다</h1>
+      <p className="block text-center">
+         링크가 잘못 되었거나 페이지가 삭제되었습니다{" "}
+        <Link to="/" className="text-blue-500">돌아가기</Link>
+      </p>
+    </div>  
+  )
+}
