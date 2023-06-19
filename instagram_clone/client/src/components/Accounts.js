7d56e4faede4bf90b096e5373644edf812750f3b
kimeumpyo <eumpyo0409@gmail.com>
2023-06-19 19:41:08 +0900

    Front

--- instagram_clone/client/src/components/Accounts.js
+++ instagram_clone/client/src/components/Accounts.js
@@ -1,1 +1,179 @@
-export default function Accounts(){}
+import { useState, useContext, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { updateUser } from "../utils/requests";
+import AuthContext from "./AuthContext";
+
+export default function Accounts() {
+  // 로그인 유저
+  const { user, setUser } = useContext(AuthContext);
+  // 업데이트할 유저의 정보를 저장한다
+  const [updatedUser, setUpdatedUser] = useState({});
+
+  console.log(updatedUser) // 업데이트 정보 추적
+
+  
+
+  // 폼 제출처리
+  async function handleSubmit(e) {
+    try {
+      e.preventDefault();
+
+      // 서버에 전송할 데이터
+      console.log(updatedUser)
+
+      // 업데이트할 정보를 폼데이터에 저장한다
+      const formData = new FormData();
+
+      Object.keys(updatedUser).forEach(prop => {
+        // formDate.append(key, value)
+        formData.append(prop, updatedUser[prop]);
+      })
+
+      // 서버에 폼데이터를 전송한다
+      const data = await updateUser(formData);
+
+      // 유저를 업데이트한다
+      setUser(data.user);
+
+      // 변경사항을 초기화한다
+      setUpdatedUser({})
+
+      alert("변경사항이 저장되었습니다")
+
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  // 파일 처리
+  function handleFile(e) {
+    const file = e.target.files[0];
+
+    if (file) {
+      // avatar의 속성값으로 파일을 추가한다
+      setUpdatedUser({ ...updatedUser, avatar: file });
+    }
+  }
+
+  // 인풋 데이터 처리
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    console.log(name, value)
+
+    // 수정이 취소된 속성을 제거한다
+    if (user[name] === value) {
+      const { [name]: value, ...rest } = updatedUser;
+      return setUpdatedUser(rest);
+    }
+
+    setUpdatedUser({ ...updatedUser, [name]: value });
+  }
+
+  // 타이틀 업데이트
+  useEffect(() => {
+    document.title = '정보 수정 - Instagram';
+  }, [])
+
+  console.log(updatedUser);
+
+  return (
+    <div className="mt-8 px-4">
+      {/* 변경사항을 알려주는 메시지 */}
+      {Object.keys(updatedUser).length > 0 && (
+        <p className="mb-4 bg-blue-500 text-white px-2 py-1">
+          변경사항을 저장하시려면 제출을 클릭하세요
+        </p>
+      )}
+
+      {/* 프로필 사진 */}
+      <div className="flex mb-4">
+        <img
+          src={updatedUser.avatar ? URL.createObjectURL(updatedUser.avatar) : `${process.env.REACT_APP_SERVER}/files/profiles/${user.avatar}`}
+          className="w-16 h-16 object-cover rounded-full border"
+        />
+        <div className="flex flex-col grow items-start ml-4">
+          <h3>{user.username}</h3>
+
+          <label className="text-sm font-semibold text-blue-500 cursor-pointer">
+            <input
+              type="file"
+              className="hidden"
+              onChange={handleFile}
+              accept="image/png, image/jpg, image/jpeg"
+            />
+            사진 업로드
+          </label>
+        </div>
+      </div>
+
+      {/* 폼 */}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label htmlFor="fullName" className="block font-semibold">Name</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            className="border px-2 py-1 rounded w-full"
+            defaultValue={user.fullName}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-2">
+          <label htmlFor="username" className="block font-semibold">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="border px-2 py-1 rounded w-full read-only:bg-gray-100"
+            defaultValue={user.username}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-2">
+          <label htmlFor="email" className="block font-semibold">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="border px-2 py-1 rounded w-full read-only:bg-gray-100"
+            defaultValue={user.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-2">
+          <label htmlFor="bio" className="block font-semibold">Bio</label>
+          <textarea
+            id="bio"
+            rows="3"
+            name="bio"
+            className="border px-2 py-1 rounded w-full"
+            defaultValue={user.bio}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex">
+          <button
+            type="submit"
+            className="text-sm font-semibold bg-gray-200 rounded-lg px-4 py-2 disabled:opacity-[0.2]"
+            disabled={Object.keys(updatedUser).length < 1}
+          >
+            저장
+          </button>
+          <Link
+            to={`/profiles/${user.username}`}
+            className="text-sm font-semibold bg-gray-200 rounded-lg px-4 py-2 ml-2"
+          >
+            취소
+          </Link>
+        </div>
+      </form>
+    </div>
+  )
+}
--- instagram_clone/client/src/components/Profile.js
+++ instagram_clone/client/src/components/Profile.js
@@ -1,1 +1,204 @@
-export default function Profile(){}
+import { useState, useEffect, useContext, useRef } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import AuthContext from "./AuthContext";
+import ArticleCreate from "./ArticleCreate";
+import Timeline from "./Timeline";
+import { getProfile, getTimeline, follow, unfollow } from "../utils/requests";
+import Spinner from "./Spinner";
+
+export default function Profile() {
+
+  const { username } = useParams();
+  const { user, setUser } = useContext(AuthContext);
+  const isMaster = user.username === username;
+  const [profile, setProfile] = useState(null);
+  const [articles, setArticles] = useState(null);
+  const [articleCount, setArticleCount] = useState(0);
+  const [active, setActive] = useState(false);
+  const navigate = useNavigate();
+
+  console.log(profile)
+
+  useEffect(() => {
+    setProfile(null);
+
+    Promise.all([
+      getProfile(username),
+      getTimeline(username)
+    ])
+      .then(([profileData, timelineData]) => {
+        setProfile(profileData.profile);
+
+        setArticles(timelineData.articles);
+        setArticleCount(timelineData.articleCount)
+      })
+      .catch(error => {
+        // NotFound 페이지로 이동한다
+        navigate('/notfound', { replace: true });
+      })
+
+  }, [username]);
+
+  // 로그아웃 처리
+  function handleSignOut() {
+    const confirmed = window.confirm('로그아웃 하시겠습니까?');
+
+    if (confirmed) {
+      setUser(null);
+    }
+  }
+
+  // 팔로우 처리
+  async function handleFollow() {
+    try {
+      await follow(username)
+
+      setProfile({ ...profile, isFollowing: true })
+
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  // 언팔로우 처리
+  async function handleUnfollow() {
+    try {
+      await unfollow(username)
+
+      setProfile({ ...profile, isFollowing: false });
+
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  // 타이틀 업데이트
+  useEffect(() => {
+    document.title = `${username} - Instagram`
+  }, [])
+
+  if (!profile) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <div className="px-4 mt-8">
+        {/* Profile Image and Info */}
+        <div className="flex mb-4">
+          <img
+            src={`${process.env.REACT_APP_SERVER}/files/profiles/${profile.avatar}`}
+            className="w-20 h-20 object-cover border rounded-full"
+          />
+
+          <div className="grow ml-4">
+            <div className="flex items-center mb-4">
+              <h3>{profile.username}</h3>
+
+              {isMaster && (
+                <div className="flex ml-2">
+                  <Link to="/accounts/edit" className="bg-gray-200 rounded-lg px-4 py-2 text-sm font-semibold">
+                    정보수정
+                  </Link>
+
+                  <button
+                    className="ml-2 bg-gray-200 px-4 py-2 text-sm font-semibold rounded-lg"
+                    onClick={handleSignOut}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                      className="w-4"
+                    >
+                      <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
+                    </svg>
+                  </button>
+                </div>
+              )}
+
+              {(!isMaster && profile.isFollowing) && (
+                <button
+                  className="ml-2 bg-gray-200 text-sm px-4 py-2 font-semibold p-2 rounded-lg"
+                  onClick={handleUnfollow}
+                >
+                  Following
+                </button>
+              )}
+
+              {(!isMaster && !profile.isFollowing) && (
+                <button
+                  className="ml-2 bg-blue-500 text-white text-sm px-4 py-2 font-semibold p-2 rounded-lg"
+                  onClick={handleFollow}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+
+            <ul className="flex items-center">
+              <li className="w-1/3">
+                <div className="text-sm">
+                  <span className="font-semibold">
+                    {profile.articleCount}
+                  </span>
+                  {" "}
+                  게시물
+                </div>
+              </li>
+              <li className="w-1/3">
+                <Link to={`/profiles/${username}/followers`} className="block text-sm">
+                  <span className="font-semibold">
+                    {profile.followerCount}
+                  </span>
+                  {" "}
+                  팔로워
+                </Link>
+              </li>
+              <li className="w-1/3">
+                <Link to={`/profiles/${username}/following`} className="block text-sm">
+                  <span className="font-semibold">
+                    {profile.followingCount}
+                  </span>
+                  {" "}
+                  팔로잉
+                </Link>
+              </li>
+            </ul>
+
+            {/* 게시물 업로드 버튼 */}
+            <svg
+              className="w-6 fixed right-8 bottom-8 hover:scale-110 cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              onClick={() => setActive(true)}
+            >
+              <path d="M200 344V280H136C122.7 280 112 269.3 112 256C112 242.7 122.7 232 136 232H200V168C200 154.7 210.7 144 224 144C237.3 144 248 154.7 248 168V232H312C325.3 232 336 242.7 336 256C336 269.3 325.3 280 312 280H248V344C248 357.3 237.3 368 224 368C210.7 368 200 357.3 200 344zM0 96C0 60.65 28.65 32 64 32H384C419.3 32 448 60.65 448 96V416C448 451.3 419.3 480 384 480H64C28.65 480 0 451.3 0 416V96zM48 96V416C48 424.8 55.16 432 64 432H384C392.8 432 400 424.8 400 416V96C400 87.16 392.8 80 384 80H64C55.16 80 48 87.16 48 96z" />
+            </svg>
+          </div>
+        </div>
+
+        {/* 이름, 자기소개 */}
+        {profile.fullName && (
+          <h3 className="text-sm font-semibold my-2">{profile.fullName}</h3>
+        )}
+        {profile.bio && (
+          <p className="text-sm my-2">
+            {profile.bio}
+          </p>
+        )}
+      </div>
+
+      <hr className="mt-4 mb-4" />
+
+      <Timeline
+        articles={articles}
+        articleCount={articleCount}
+      />
+
+      <ArticleCreate
+        active={active}
+        setActive={setActive}
+      />
+    </>
+  )
+}
